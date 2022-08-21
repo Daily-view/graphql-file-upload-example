510e9dafb033881a2e1a34c87e5a7627e10ed4aa
nothingprogram <popawaw@naver.com>
2022-08-21 16:35:21 +0900

    graphql file upload example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createUploadLink } from 'apollo-upload-client'
+import {ApolloClient} from "apollo-client"
+import {ApolloProvider} from "react-apollo"
+import gql from "graphql-tag"
+import { Mutation } from "react-apollo"
+
+const apolloCache = new InMemoryCache()
+
+const uploadLink = createUploadLink({
+  uri: 'http://localhost:9000/graphql',
+  headers: {
+    "keep-alive": "true",
+    "Content-Type": "application/json"
+  }
+})
+
+const client = new ApolloClient({
+  cache: apolloCache,
+  link: uploadLink
+})
+
+const fileUpload = ({target: { files }}) => {
+  const file = files[0]
+  console.log(file)
+}
+
+const UPLOAD_FILE = gql`
+    mutation uploadFile($input: Upload!) {
+    uploadFile(input: input)
+    }
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApolloProvider client={client}>
+        <header className="App-header">
+          <h2>Save Local</h2>
+          <Mutation mutation={UPLOAD_FILE}>
+            {(singleUpload, { data, loading }) => {
+              console.log(data)
+              return (<form onSubmit={fileUpload} encType={'multipart/form-data'}>
+                <input name={'document'} type={'file'} onChange={({target: { files }}) => {
+                  const file = files[0]
+                  file && singleUpload({ variables: { file: file } })
+                }}/>
+                {loading && <p>Loading.....</p>}
+              </form>)
+            }
+            }
+          </Mutation>
+          <h2>Stream to Server</h2>
+        </header>
+      </ApolloProvider>
     </div>
   );
 }
